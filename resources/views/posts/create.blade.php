e3e2b3c563fa080c833be7cfe2c6d213aed45c7e
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-10 08:54:58 +0900

    add image preview

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -27,10 +27,20 @@
                 <label class="block text-gray-700 text-sm mb-2" for="image">
                     ブログ用画像
                 </label>
-                <input type="file" name="image" class="border-gray-300">
+                <input type="file" name="image" class="border-gray-300" onchange="previewImage(this);">
+                <img id="preview" style="max-width:200px;">
             </div>
             <input type="submit" value="登録"
                 class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
         </form>
     </div>
+    <script>
+        function previewImage(obj) {
+            var fileReader = new FileReader();
+            fileReader.onload = (function() {
+                document.getElementById('preview').src = fileReader.result;
+            });
+            fileReader.readAsDataURL(obj.files[0]);
+        }
+    </script>
 </x-app-layout>
